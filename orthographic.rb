e60dc56611a38e8754c17af69c05037b0e7b517b
Norbert Reschke <reschke@hsbkob.de>
2022-02-18 10:06:54 +0100

    geändert:       orthographic.rb

--- orthographic.rb
+++ orthographic.rb
@@ -59,7 +59,9 @@ canvas.rotate(90) {
 
 # begin frame
   canvas.font('Helvetica', size: 8)
-  canvas.text( "Produkterstellung - Technische Zeichnungen - in der Ausbildungsvorbereitung", at:[mm(30),mm(291)])
+  canvas.text( "Produkterstellung - Technische Zeichnungen - in der Ausbildungsvorbereitung", at:[mm(25),mm(291.5)])
+  canvas.text( "#{File.basename(__FILE__,'.rb')}.pdf", at:[mm(180),mm(291.5)])
+
 canvas.line_width(2) {
   canvas.line_cap_style = 2
   canvas.line(mm(25),mm(7),mm(200),mm(7)).stroke
@@ -68,6 +70,14 @@ canvas.line_width(2) {
   canvas.line(mm(25),mm(7),mm(25),mm(290)).stroke
   canvas.line(mm(200),mm(7),mm(200),mm(290)).stroke
 }
+canvas.font('Helvetica', size: 15)
+canvas.text( "3-Tafel-Projektion", at:[mm(27),mm(20)])
+canvas.font('Helvetica', size: 8)
+canvas.text( "Vorname", at:[mm(27),mm(15)])
+canvas.text( "Name", at:[mm(60),mm(15)])
+canvas.text( "Nummer", at:[mm(100),mm(15)])
+canvas.text( "Klasse", at:[mm(130),mm(15)])
+canvas.text( "Datum", at:[mm(170),mm(15)])
 # end frame
 
 doc.write("#{File.basename(__FILE__,'.rb')}.pdf", optimize: true)
